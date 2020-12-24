547ab9d0ec3d99061e1e2466ac8bbc5e5540476d
Nurmuhammad Ergashev <nurmuhammad@kpi.com>
2020-12-24 18:24:58 +0500

    Changes of Img

--- src/components/Products/data.js
+++ src/components/Products/data.js
@@ -1,7 +1,9 @@
 import product1 from '../../images/product-1.jpg'
 import product2 from '../../images/product-2.jpg'
 import product3 from '../../images/product-3.jpg'
-
+import product4 from '../../images/product-4.jpg'
+import product5 from '../../images/product-5.jpg'
+import product6 from '../../images/product-6.jpg'
 
 export const productData = [
     {
@@ -32,7 +34,7 @@ export const productData = [
 
 export const productDataTwo = [
     {
-        img: product1,
+        img: product4,
         alt: 'Pizza',
         name: 'Supreme Pizza',
         desc: 'Supreme Pizza one of the delicious Pizza',
@@ -40,7 +42,7 @@ export const productDataTwo = [
         button: 'Add to Cart'
     },
     {
-        img: product2,
+        img: product5,
         alt: 'Pizza',
         name: 'Margarito Pizza',
         desc: 'Supreme Pizza one of the delicious Pizza',
@@ -48,7 +50,7 @@ export const productDataTwo = [
         button: 'Add to Cart'
     },
     {
-        img: product3,
+        img: product6,
         alt: 'Pizza',
         name: 'Tatta Pizza',
         desc: 'Tatta Pizza one of the delicious Pizza',
